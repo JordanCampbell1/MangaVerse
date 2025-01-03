8f70b12bf23ce1bc49fd1b292e2452b40c9776bb
Jordan Campbell <jordancampbell.work@gmail.com>
2025-01-03 11:14:23 -0500

    categories front end fairly complete

--- mangaverse/src/pages/Categories/Categories.jsx
+++ mangaverse/src/pages/Categories/Categories.jsx
@@ -1,9 +1,22 @@
-import './Categories.css'
+import "./Categories.css";
 
 const Categories = () => {
-    return(
-        <h1>Categories Page</h1>
-    );
-}
+  return (
+    <div className="grid-container">
+      <p>Action</p>
+      <p>Adventure</p>
+      <p>Comedy</p>
+      <p>Cooking</p>
+      <p>Drama</p>
+      <p>Fantasy</p>
+      <p>Isekai</p>
+      <p>Historical</p>
+      <p>Horror</p>
+      <p>Sports</p>
+      <p>Supernatural</p>
+      <p>Psychological</p>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
