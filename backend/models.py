85e958449a78e30fdc2a03d84dc5ccf878c51635
Jordan Campbell <jordancampbell.work@gmail.com>
2025-05-28 03:07:33 -0500

    made fastapi routes for the requests needed for the react frontend

--- backend/models.py
+++ backend/models.py
@@ -7,5 +7,5 @@ class Manga(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    author = Column(String, index=True)
+    imageURL = Column(String, index=True)
     # add your other columns here
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,17 +1,49 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+from typing import List, Optional
 
 
 class MangaBase(BaseModel):
+    id: str
     title: str
-    author: str
+    imageURL: HttpUrl
 
 
-class MangaCreate(MangaBase):
-    pass
+class Chapter(BaseModel):
+    id: str
+    chapter: Optional[str]
+    title: Optional[str]
 
 
-class MangaRead(MangaBase):
-    id: int
+class MangaDetail(BaseModel):
+    id: str
+    title: str
+    description: Optional[str]
+    imageURL: str
+    chapters: List[Chapter]
 
-    class Config:
-        orm_mode = True
+
+class ChapterImageResponse(BaseModel):
+    id: str
+    title: str
+    imageURLs: List[str]
+    chapterNumber: Optional[str]
+
+
+class MangaChapterView(BaseModel):
+    mangaID: str
+    mangaTitle: str
+    coverImage: str
+    selectedChapter: ChapterImageResponse
+
+
+class MangaSearchResult(BaseModel):
+    id: str
+    title: str
+    description: str
+    image: str
+
+
+class MangaBasicInfo(BaseModel):
+    id: str
+    title: str
+    imageUrl: Optional[str]
