37c698c22d01426cb510e76cfbea25c215e10920
Jordan Campbell <jordancampbell.work@gmail.com>
2025-06-09 23:41:05 -0500

    updated log for possible invalid ilenames

--- backend/rag_api/utils/logging_utils.py
+++ backend/rag_api/utils/logging_utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import datetime
 
 
@@ -8,7 +9,15 @@ def log_prompt(prompt: str, query: str):
 
     # Sanitize and create a timestamped filename
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    safe_query = query.replace(" ", "_").replace("/", "_")[:50]  # limit filename length
+
+    # Remove or replace unsafe characters in the query for filename
+    safe_query = re.sub(
+        r'[<>:"/\\|?*\n\r\t]', "_", query
+    )  # replace disallowed characters
+    safe_query = safe_query.replace(" ", "_")[
+        :50
+    ]  # replace spaces and limit filename length
+
     filename = f"prompt_logs/{timestamp}_{safe_query}.txt"
 
     # Write the prompt to the file
